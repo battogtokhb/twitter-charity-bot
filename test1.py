813c8bdb3f2343ad2c61027471c12797302a5bbf
Michael Basili <mbasili26@gmail.com>
2019-07-01 23:24:47 -0700

    Slightly improved twitter streaming

--- test1.py
+++ test1.py
@@ -6,6 +6,8 @@ import authentication as auth
 
 # Test: tweet out a message to our Twitter account
 
+# myAuth = TwitterAuthenticator().authenticate()
+# api = tweepy.API(myAuth)
 # for tweet in tweepy.Cursor(api.search, q='tweepy').items(10):
 #     print(tweet.text)
 
@@ -24,25 +26,34 @@ class MyStreamListener(StreamListener):
         return True
 
     def on_error(self, status_code):
+        if status == 420:
+            # Return false if we have a rate limit issue
+            return False
         print(status_code)
 
+# Class for authenticating
+class TwitterAuthenticator():
 
-class TwitterStreamer():
-    # Class for finding and processing live tweets
-    def StreamTweets(self, track_list):
-        # This handles authentication and connection to the Twitter API
+    def authenticate(self):
         # note: might need to change this to tweepy.AppAuthHandler
         twitterAuth = tweepy.OAuthHandler(auth.consumer_key, auth.consumer_secret)
         twitterAuth.set_access_token(auth.access_token, auth.token_secret)
-        api = tweepy.API(twitterAuth)
+        return twitterAuth
 
+
+class TwitterStreamer():
+    def __init__(self):
+        self.twitter_auth = TwitterAuthenticator()
+
+    # For finding and processing live tweets
+    def StreamTweets(self, track_list):
+        myAuth = self.twitter_auth.authenticate()
         # create stream listener and filter by the track_list
         myStreamListener = MyStreamListener()
-        myStream = tweepy.Stream(twitterAuth, myStreamListener)
-
+        myStream = tweepy.Stream(myAuth, myStreamListener)
         myStream.filter(track=track_list)
 
-# Test if this code works so far
-track_list = ['Joe Biden', 'Julian Castro', 'Bernie Sanders']
+# Test if this code works so far (spoiler alert: it works (for me at least))
+track_list = ['RT,donate']
 myStreamer = TwitterStreamer()
 myStreamer.StreamTweets(track_list)
